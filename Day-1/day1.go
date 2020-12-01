1a96d73a532dd5fd19be1f624ab9d22b5f82a7e1
Natalie Girard <girardnatalie@gmail.com>
2020-12-01 01:43:42 -0500

    Day 1

---
+++ Day-1/day1.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+)
+
+func readFile(filename string) []int {
+	lines := make([]int, 0)
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		l := scanner.Text()
+		line, _ := strconv.Atoi(l)
+		lines = append(lines, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return lines
+}
+
+func sumTwo(line []int) int {
+	bottom := 0
+	top := len(line) - 1
+	sum := line[bottom] + line[top]
+
+	for sum != 2020 {
+		if sum > 2020 {
+			top--
+		} else {
+			bottom++
+		}
+		sum = line[bottom] + line[top]
+	}
+
+	return line[bottom] * line[top]
+}
+
+func sumThree(line []int) int {
+	for i := 0; i < len(line); i++ {
+		a := line[i]
+		bc := 2020 - a
+
+		for j := i + 1; j < len(line); j++ {
+			b := line[j]
+			if b >= bc {
+				break
+			}
+
+			c := 2020 - (a + b)
+			for k := 0; k < len(line); k++ {
+				if line[k] == a || line[k] == b {
+					continue
+				}
+				if line[k] == c {
+					return a * b * c
+				}
+			}
+		}
+	}
+	return 0
+}
+
+func main() {
+	line := readFile("input.txt")
+	sort.Ints(line)
+
+	twoProduct := sumTwo(line)
+	fmt.Println("Part 1:", twoProduct)
+
+	threeProduct := sumThree(line)
+	fmt.Println("Part 2:", threeProduct)
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Advent of Code 2020
+
+Completed exercises by Natalie Girard using GoLang.
+
+## How to run
+
+To run the code for a specific day, go to that directory and run go program and filename as first argument.
+
+Ex: `go run day1.go input.txt`
